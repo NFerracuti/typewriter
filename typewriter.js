476322ebe28f1ffa11c44d64f29e09c477abdb53
NFerracuti <npferracuti@gmail.com>
2023-05-13 19:53:12 -0400

    implemented the typewriter function

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+
+  if (i === sentence.length - 1) {
+    setTimeout(() => {
+      process.stdout.write(sentence[i] + '\n');
+    }, i * 50);
+  } else {
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, i * 50);
+  } 
+};
+
+
